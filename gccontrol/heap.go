f9a72997f00728d5f73a5a3d2723b23bbbb5915a
Daniel Fireman <danielfireman@gmail.com>
2017-12-08 10:40:41 -0300

    Fix monotonic increasing counter in heap.go. Improve code documentation and add tests to heap.go. WIP #1

--- gccontrol/heap.go
+++ gccontrol/heap.go
@@ -7,59 +7,97 @@ import (
 
 const (
 	// Default heap threshold rate should be fairly small, so the first collection happens quickly.
-	defaultSheddingThreshold = 50 * 1e6
+	defaultSheddingThreshold = uint64(50 * 1e6)
 
-	maxSheddingThreshold = 500 * 1e6
+	// There is no special reason for this constant.
+	// TODO(gcinterceptor): https://github.com/gcinterceptor/gci-go/issues/3
+	maxSheddingThreshold = uint64(512 * 1e6)
 )
 
+// rt is a tiny interface around the runtime to make tests easier.
+type rt interface {
+	// HeapAlloc is bytes of allocated heap objects.
+	// From runtime#Memstats:
+	// "Allocated" heap objects include all reachable objects, as
+	// well as unreachable objects that the garbage collector has
+	// not yet freed. Specifically, HeapAlloc increases as heap
+	// objects are allocated and decreases as the heap is swept
+	// and unreachable objects are freed. Sweeping occurs
+	// incrementally between GC cycles, so these two processes
+	// occur simultaneously, and as a result HeapAlloc tends to
+	// change smoothly (in contrast with the sawtooth that is
+	// typical of stop-the-world garbage collectors).
+	HeapAlloc() uint64
+	// GC runs a garbage collection and blocks the caller until the garbage
+	// collection is complete. It may also block the entire program.
+	GC()
+}
+
+// goRT is the default Go implementation of rt.
+type goRT struct{}
+
+func (r *goRT) HeapAlloc() uint64 {
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+	return mem.HeapAlloc
+}
+
+func (r *goRT) GC() {
+	runtime.GC()
+}
+
 type heap struct {
-	count      int
-	past       []uint64
-	st         uint64 // Shedding treshold.
-	lastCheck  uint64 // Keeping allocated heap from last check. This avoid read memory stats more than neeeded.
-	cleanSlate uint64 // Heap size just after being cleaned.
+	next       int      // Next index in the past slice.
+	past       []uint64 // History of heap consumption between collections.
+	st         uint64   // Shedding treshold.
+	cleanSlate uint64   // Heap size just after being cleaned.
+	rt         rt       // System runtime.
 }
 
 func newHeap() *heap {
 	return &heap{
-		st: defaultSheddingThreshold,
-
 		// The amount of resources to compute each request can vary
 		// a lot over time, keeping a long history might not improve the decision.
 		past: []uint64{0, 0, 0, 0, 0},
+		st:   defaultSheddingThreshold,
+		rt:   &goRT{},
 	}
 }
 
+// check verifies if the current amount of heap consumed processing requests is bigger than the shedding threshold.
 func (h *heap) check() bool {
-	var mem runtime.MemStats
-	runtime.ReadMemStats(&mem)
-	h.lastCheck = mem.Alloc
-	return (h.lastCheck - h.cleanSlate) >= atomic.LoadUint64(&h.st)
+	return (h.rt.HeapAlloc() - h.cleanSlate) >= atomic.LoadUint64(&h.st)
 }
 
+// collect performs a garbage collection. It also updates the shedding threshold based on amount of heap
+// consumed during request processing since the last GC.
 func (h *heap) collect() {
-	defer func() { h.count++ }()
-	runtime.GC()
+	// Calculating the amount of heap consumed by request processing.
+	// Notice that (if there in no memory leak) the amount of heap after GC is what the
+	// is used to keep the server up. So called, the clean slate.
+	allocBeforeGC := h.rt.HeapAlloc()
+	h.rt.GC()
+	allocAfterGC := h.rt.HeapAlloc()
 
-	var mem runtime.MemStats
-	runtime.ReadMemStats(&mem)
-	h.cleanSlate = mem.HeapAlloc
+	// Update the history with the memory consumed processing requests.
+	h.past[h.next] = allocBeforeGC - allocAfterGC
+	h.next = (h.next + 1) % len(h.past)
 
-	// Update the history.
-	h.past[h.count%len(h.past)] = h.lastCheck - h.cleanSlate
-
-	// Get the maximum value.
-	max := h.past[0]
-	for i := 1; i < len(h.past); i++ {
-		if h.past[i] > max {
-			max = h.past[i]
-		}
-	}
-
-	// Apply bounds.
+	// Updating the shedding threshold.
+	max := maxUint64(h.past)
 	if max > maxSheddingThreshold {
 		atomic.StoreUint64(&h.st, maxSheddingThreshold)
 	} else {
 		atomic.StoreUint64(&h.st, max)
 	}
+}
+
+func maxUint64(s []uint64) uint64 {
+	max := uint64(0)
+	for _, v := range s {
+		if v > max {
+			max = v
+		}
+	}
+	return max
 }
