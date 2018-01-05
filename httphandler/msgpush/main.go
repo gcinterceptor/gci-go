ba54a55570fb4bded8c6daec2ef91727b5678ad6
Daniel Fireman <danielfireman@gmail.com>
2018-01-05 11:51:04 -0300

    Add a msgpush handler example, which will be used in experiments

---
+++ httphandler/msgpush/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/gcinterceptor/gci-go/httphandler"
+)
+
+var (
+	useGCI   = flag.Bool("use_gci", false, "Whether to use GCI.")
+	msgSize  = flag.Int64("msg_size", 10*1024, "Number of bytes to be allocated in each message.")
+	msgCount = int64(0)
+)
+
+var messagePush = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	const windowSize = 200000
+	var buffer [windowSize][]byte
+	m := make([]byte, *msgSize)
+	for i := range m {
+		m[i] = byte(i)
+	}
+	buffer[msgCount%windowSize] = m
+	msgCount++
+})
+
+func main() {
+	flag.Parse()
+	http.Handle("/", httphandler.GCI(messagePush))
+	http.ListenAndServe(":3000", nil)
+	fmt.Println("hello")
+}
