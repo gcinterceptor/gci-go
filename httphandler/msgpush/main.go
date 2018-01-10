9195e647ebb4f31c32f8e52192590f325aee0d2f
Daniel Fireman <danielfireman@gmail.com>
2018-01-10 09:50:21 -0300

    Including metrics collection to httphandler example

--- httphandler/msgpush/main.go
+++ httphandler/msgpush/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"flag"
+	"fmt"
 	"net/http"
+	"os"
+	"runtime"
+	"sync/atomic"
+	"time"
 
 	"github.com/gcinterceptor/gci-go/httphandler"
 )
@@ -13,10 +20,10 @@ const (
 )
 
 var (
-	useGCI   = flag.Bool("use_gci", false, "Whether to use GCI.")
-	msgSize  = flag.Int64("msg_size", 10*1024, "Number of bytes to be allocated in each message.")
-	msgCount = int64(0)
-	workQueue  = make(chan struct{}, queueSize)
+	useGCI    = flag.Bool("use_gci", false, "Whether to use GCI.")
+	msgSize   = flag.Int64("msg_size", 10*1024, "Number of bytes to be allocated in each message.")
+	msgCount  = int64(0)
+	workQueue = make(chan struct{}, queueSize)
 )
 
 var messagePush = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +43,7 @@ func main() {
 					m[i] = byte(i)
 				}
 				buffer[msgCount%windowSize] = m
-				msgCount++
+				atomic.AddInt64(&msgCount, 1)
 			}
 		}()
 	}
@@ -45,5 +52,39 @@ func main() {
 	} else {
 		http.Handle("/", messagePush)
 	}
+
+	go func() {
+		f, err := os.Create("metrics.csv")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[Warning] Error creating metrics output file: %q", err)
+			return
+		}
+		defer f.Close()
+		w := csv.NewWriter(bufio.NewWriter(f))
+		defer w.Flush()
+
+		metrics := []string{
+			"numRequestsTotal",
+			"allocBytes",
+			"pauseTotalNs",
+			"numGCTotal",
+			"numForcedGCTotal",
+		}
+		w.Write(metrics)
+		w.Flush()
+		for _ = range time.Tick(1 * time.Second) {
+			var memStats runtime.MemStats
+			runtime.ReadMemStats(&memStats) // This takes 50-200us.
+			w.Write([]string{
+				fmt.Sprintf("%d", atomic.LoadInt64(&msgCount)),
+				fmt.Sprintf("%d", memStats.Alloc),
+				fmt.Sprintf("%d", memStats.TotalAlloc),
+				fmt.Sprintf("%d", memStats.NumGC),
+				fmt.Sprintf("%d", memStats.NumForcedGC),
+			})
+			w.Flush()
+		}
+	}()
+
 	http.ListenAndServe(":3000", nil)
 }
