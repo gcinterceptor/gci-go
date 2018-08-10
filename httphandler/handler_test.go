8e5cd15ea4ff79ddd0c70a6f2a781e9b2eed301e
Daniel Fireman <danielfireman@gmail.com>
2018-08-10 15:52:16 -0300

    Improving handler tests

--- httphandler/handler_test.go
+++ httphandler/handler_test.go
@@ -2,12 +2,15 @@ package httphandler
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"testing"
 )
 
-func ExampleIntercept() {
+func ExampleGCI() {
 	f := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hi")
 	})
@@ -26,4 +29,48 @@ func ExampleIntercept() {
 	}
 	fmt.Print(string(b))
 	// Output: Hi
+}
+
+var handlerMessage = "Foo"
+
+func defaultTestHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, handlerMessage)
+}
+
+func TestGCI_checkHeader(t *testing.T) {
+	handler := GCI(defaultTestHandler)
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	req.Header.Set(gciHeader, heapCheckHeader)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	body, _ := ioutil.ReadAll(w.Result().Body)
+	_, err := strconv.Atoi(string(body))
+	if err != nil {
+		t.Fatalf("want:number got err:%q", err)
+	}
+}
+
+func TestGCI_fallthrough(t *testing.T) {
+	handler := GCI(defaultTestHandler)
+	w := httptest.NewRecorder()
+	handler(w, httptest.NewRequest("GET", "http://example.com/foo", nil))
+
+	body, _ := ioutil.ReadAll(w.Result().Body)
+	if string(body) != handlerMessage {
+		t.Fatalf("want:%s got:%s", handlerMessage, string(body))
+	}
+}
+
+func TestGCI_gc(t *testing.T) {
+	handler := GCI(defaultTestHandler)
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	req.Header.Set(gciHeader, "gen1")
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	body, _ := ioutil.ReadAll(w.Result().Body)
+	if string(body) != "" {
+		t.Fatalf("want:\"\" got:%s", string(body))
+	}
 }
